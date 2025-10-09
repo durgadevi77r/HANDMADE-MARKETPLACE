34f4ee6ef258f56dff4c651599272c04bd939f95
Dhivya <jmdhivya2005@gmail.com>
2025-10-09 14:54:05 +0530

    Fixed

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/HANDMADE-MARKETPLACE'
+  base: process.env.VITE_BASE_URL || '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser'
+  }
 })
